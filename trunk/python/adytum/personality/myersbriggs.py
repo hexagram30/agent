68a6da574737b61fb0d5d8ed1d5b0f218a009495
Duncan McGreggor <oubiwann@twistedmatrix.com>
2004-09-12 07:19:02 +0000

    * Renamed getTemperment* functions. * Created a temperment master list generator. * Added getGeneticTemperment. * Removed the flipIndex() function since it wasn't being used. * Rewrote getMatchTypes(); it actually works now. * Clarified the methodology behind getMatchType() with an extensive docstring. * Added getDominantFunction(). * Created a new generator.BasePerson class. * Added an empty place-holder for interfaces. * Added package place-holders for behavior patterns and environments.

--- trunk/python/adytum/personality/myersbriggs.py
+++ trunk/python/adytum/personality/myersbriggs.py
@@ -36,14 +36,11 @@ TG = ['ENTJ', 'INTJ', 'ENTP', 'INTP']
 FG = ['ENFJ', 'INFJ', 'ENFP', 'INFP']
 
 # Dominant Functions
-EN = ['ENTP', 'ENFP']
-ES = ['ESTP', 'ESFP']
-EF = ['ESFJ', 'ENFJ']
-ET = ['ESTJ', 'ENTJ']
-IN = ['INTJ', 'INFJ']
-IS = ['ISTJ', 'ISFJ']    
-IF = ['ISFP', 'INFP']
-IT = ['ISTP', 'INTP']
+NDF = ['ENTP', 'ENFP', 'INTJ', 'INFJ']
+SDF = ['ESTP', 'ESFP', 'ISTJ', 'ISFJ']
+FDF = ['ESFJ', 'ENFJ', 'ISFP', 'INFP']
+TDF = ['ESTJ', 'ENTJ', 'ISTP', 'INTP']
+DOM_FUNC = {'N':NDF, 'S':SDF, 'F':FDF, 'T':TDF}
 
 POPULATION_MATRIX = array()
 
@@ -56,6 +53,13 @@ def getIndexForLetter(letter):
     '''
     return [ letter.upper() in pair for pair in RP ].index(True)
 
+def getValueForLetter(letter):
+    '''
+    This function doesn't care what grouping (index of RP) the letter is
+    in, it just returns a 1 or -1, based on the constants defined above.
+    '''
+    return eval(letter)
+
 def getLetterForValue(letter_value, pair_index):
     '''
     '''
@@ -65,7 +69,7 @@ def getLetterForValue(letter_value, pair_index):
     
 def getProperOrder(type_abbr):
     '''
-    Order the input temperment type according to the order
+    Order the input temperament type according to the order
     defined in the reference pairs.
     '''
     # setup a list with the proper length so that indices get
@@ -78,51 +82,118 @@ def getProperOrder(type_abbr):
     [ (new.insert(getIndexForLetter(letter), letter), new.pop(getIndexForLetter(letter) + 1)) for letter in type_abbr ]
     return new
     
-def getTempermentType(type_abbr):
+def getTemperamentList(type_abbr):
     return [ eval(i.upper()) for i in getProperOrder(type_abbr) ]
 
-def getArray(type_abbr):
-    return array(getTempermentType(type_abbr))
+def getTemperamentArray(type_abbr):
+    return array(getTemperamentList(type_abbr))
 
-def getTempermentString(type_list):
+def getTemperamentString(type_list):
     return ''.join([ getLetterForValue(x[0], x[1]) for x in zip(type_list, range(0,len(type_list))) ])
 
-def flipIndex(input_list, index):
-    input_list.insert(index, input_list(index)*-1)
-    input_list.pop(index+1)
-    return input_list
+def checkTemperament(type_abbr):
+    return getTemperamentString(getTemperamentList(type_abbr))
 
-def getMatchTypes(type_abbr):
+def getDominantFunction(type_list):
     '''
-    flip indices 0 (E/I) and then combine that with a separate
-    flip of index 1 (N/S) and index 3 (J/P) 
+    Dominant functions are either in the T/F grouping or the S/P grouping.
+    This function returns the letter abbreviation of the type (one of 'T', 'F',
+    'S', or 'P').
     '''
-    if type_abbr in T:
-        #swap E/I and J/P
-    
-        # one one more, swap N/S
-        pass
-    elif type_abbr in F:
-        #        
-        pass
+    temperm = getTemperamentString(type_list)
+    for func, temperm_list in DOM_FUNC.items():
+        if temperm in temperm_list: return func
+
+def getMatchTypes(type_list):
+    '''
+    Returns a tuple of possible good matches.
+
+    The principles behind establishing "matches" or compatibilies between temperaments
+    involve two things:
+        1) temperaments that share dominant functions are well-suited for each other
+        2) temperaments that have opposite "directions" (Introversion or Extroversion) 
+        can well-suited for each other
+        3) temperaments that have opposite ways of dealing with the world can be
+        well-suited for each other.
+        4) temperaments that share "information gathering" methods (Sensing or
+        Intuition) can be well-suited for each other.
+
+    This results in the following logic for possible good matches for a given 
+    temperament:
+        * switch the first index: if it was 'E', makd it 'I' and vice versa
+        * find the dominant function, and keep that the same.
+        * flip the last index: if it was 'J', make it 'P', and vice versa
+        * there now remain two possibilities, provided be the two choices of
+        the remaining index.
+    '''
+    match1 = type_list
+    # if extrovert, make introver, and vice versa
+    match1[0] *= -1
+
+    # flip world-dealing bit (last bit)
+    match1[3] *= -1
+
+    # setup the dominant function stuff, and flip non domin bit
+    domin_func = getDominantFunction(type_list)
+    domin_func_index = getIndexForLetter(domin_func)
+    domin_func_value = getValueForLetter(domin_func)
     from copy import copy
-    match1 = getTempermentType(type_abbr)
-    match1[0] *= -1             # flip E/I for all of them
     match2 = copy(match1)
-    match3 = copy(match1)
-    #match1[1] *= -1
-    match2[2] *= -1
-    match3[3] *= -1
+    if domin_func_index == 2: match2[1] *= -1
+    else: match2[2] *= -1
 
-    return (getTempermentString(match1), getTempermentString(match2), getTempermentString(match3))
-    
-# XXX no sure is this class is really needed, yet. This module may just end up
-# being a set of functional/procedural utilities with no need for a class.
-class Temperment(object):
+    return (getTemperamentString(match1), getTemperamentString(match2))
+
+def getCompatibilityScale(person1, person2):
     '''
+    This function attempts to scale between 0 and 1 the level of compatibility
+    between two individuals, where 1 would be one of the two best matches and 0
+    represent the oposite types. Decimal values between 0 and 1 attempt to 
+    represent degree of compatibility. 
     '''
-    def __init__(self, type_abbr):
-        pass
+    pass
+
+def getCompleteTemperamentStrings():
+    # XXX This seems hackish to me, or at the least, inellegant. There must be
+    # a nice clever way to do this that involve list comprehensions, or 
+    # iterators, or generators...
+    #
+    # this is basically just binary math, whether you are using 0/1, -1/1, E/I,
+    # etc., it's all the same thing. An elegant solution will take advantage of
+    # the fact that this is binary math...
+    full_list = []
+    for i in [1,-1]:
+        for j in [1,-1]:
+            for k in [1,-1]:
+                for l in [1,-1]:
+                    full_list.append(getTemperamentString([i,j,k,l]))
+    return full_list
+
+def getGeneticTemperament(parent1, parent2):
+    '''
+    The basic jist of this function is this: I wanted some way to produce semi-
+    random "off-spring" that were the result of combinations of type types. This
+    is not meant to realistically model personality types of children based on
+    the types of their parents. However, it does offer a nice way of auto-generating
+    family trees/generations of temperaments.
+    '''
+    # generate a random "mask" list, composed of true and false booleans
+    # e.g. : [False, True, False, True]
+    from random import random
+    from math import ceil
+    mask = [ bool(int(ceil(random() * 2)) % 2) for x in range(0,4) ]
+    # pair off the ith element of each list (mask, parent1 and parent2) in a new list
+    combo = zip(mask, parent1, parent2)
+
+    # for every True in each element of the combo list, take the corresponding element 
+    # from parent1, and for every False, take the corresponding element from parent2,
+    # thus giving a new temperament that is the result of the combination of two parents.
+    def checkit(part):
+        if part[0]: return part[1]
+        return part[2]
+
+    return [ checkit(x) for x in combo ]
+
 
     def getType(self, type_abbr):
         pass
--- trunk/python/test_code.py
+++ trunk/python/test_code.py
@@ -1,10 +1,10 @@
-from numarray import outerproduct, outerproduct
+from numarray import outerproduct, outerproduct, diagonal
 from adytum.personality import myersbriggs as mb
 
 rp = mb.REFERENCE_PAIRS
-me = mb.getTempermentType('INTP')
-mg = mb.getTempermentType('ENTJ')
-el = mb.getTempermentType('INTJ')
+me = mb.getTemperamentArray('INTP')
+mg = mb.getTemperamentArray('ENTJ')
+el = mb.getTemperamentArray('INTJ')
 
 outerproduct(me, mg)
 diagonal(outerproduct(mg, me))
@@ -13,3 +13,16 @@ me + mg
 
 mb.getProperOrder('intp')
 mb.getProperOrder('pnti')
+
+for i in [1,-1]:
+  for j in [1,-1]:
+    for k in [1,-1]:
+      for l in [1,-1]:
+        mb.getTemperamentString([i,j,k,l])
+
+
+def getStats(alist):
+  seen = {0:0, 1:0}
+  [ seen.update({x:seen[x]+1}) for x in alist ]
+  return seen
+
