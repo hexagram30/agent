620facd49b92239d3b53a560e806b9914af457b1
Duncan McGreggor <oubiwann@twistedmatrix.com>
2004-09-16 01:51:08 +0000

    * Added stubs for perception, emotion. * Added code from the book "Artificial Intelligence - A Modern Approach."

---
+++ trunk/python/adytum/perception/interfaces.py
@@ -0,0 +1,9 @@
+'''
+The perception module needs to integrate several components:
+
+* agent physiology
+* agent coping style
+* prior emotional needs
+* any prior memory elements from previous 'cycles'
+
+'''
