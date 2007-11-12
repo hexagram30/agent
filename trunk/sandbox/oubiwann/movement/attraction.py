d8f27eef327b9892b9c5996d5a355bad982ed06f
Duncan McGreggor <oubiwann@twistedmatrix.com>
2007-11-12 21:56:06 +0000

    Implemented the necessary code for a motivated walk.

--- trunk/sandbox/oubiwann/movement/attraction.py
+++ trunk/sandbox/oubiwann/movement/attraction.py
@@ -29,6 +29,15 @@ class Location(object):
         return "<%s: x=%s, y=%s, z=%s>" % (
             self.__class__.__name__, self.x, self.y, self.z)
 
+    def __call__(self):
+        return (self.x, self.y, self.z)
+
+    def __iter__(self):
+        return iter([self.x, self.y, self.z])
+
+    def getCoords(self):
+        return (self.x, self.y, self.z)
+
 
 class Place(object):
     """
@@ -42,6 +51,31 @@ class Place(object):
         self.volume = w * l * h
         self.map = {}
 
+    def __contains__(self, objOrLocation):
+        """
+
+        """
+        if isinstance(objOrLocation, Location):
+            x, y, z = objOrLocation()
+            # the ranges we need to check are, for example, from 1 to width. In
+            # other words, we're not looking for indices, but rather positive
+            # quanta in a grid.
+            if (x in xrange(1, self.width+1) and y in xrange(1, self.length+1)
+                and z in xrange(1, self.height+1)):
+                return True
+            return False
+        elif isinstance(objOrLocation, Object):
+            if objOrLocation in self.map.values():
+                return True
+            return False
+        else:
+            msg = "A Place object can only check for locations or objects."
+            raise Exception, msg
+
+    def __repr__(self):
+        return "<%s: w=%s, l=%s, h=%s>" % (
+            self.__class__.__name__, self.width, self.length, self.height)
+
     def addObject(self, obj, location):
         """
 
@@ -49,8 +83,12 @@ class Place(object):
         # update map
         if isinstance(location, tuple):
             location = Location(*location)
+        if location not in self:
+            msg = "Can't add an object at a non-location."
+            raise Exception, msg
         self.map[location] = obj
         obj.location = location
+        obj.setContainer(self)
 
     def getObjectAt(self, location):
         """
@@ -76,22 +114,7 @@ class Place(object):
         print "Leaving old location %s for %s..." % (oldLoc, location)
         self.map[oldLoc] = None
         self.map[location] = obj
-
-    def __contains__(self, objOrLocation):
-        """
-
-        """
-        if isinstance(objOrLocation, Location):
-            if objOrLocation in self.map.keys():
-                return True
-            return False
-        elif isinstance(objOrLocation, Object):
-            if objOrLocation in self.map.values():
-                return True
-            return False
-        else:
-            msg = "A Place object can only check for locations or objects."
-            raise Exception, msg
+        obj.location = location
 
 
 class Room(Place):
@@ -110,15 +133,21 @@ class Relationship(dict):
         self.object = obj
         self.attraction = attraction
 
-    def getObjectDistance(self):
-        return self.owner.location - self.object.location
+    def getObjectDistance(self, location=None):
+        if not location:
+            location = self.owner.location
+        return location - self.object.location
 
 
-    def getNearestLocation(self):
+    def getNearestAdjacentLocation(self):
         """
         Examine all neighboring locations and find the one that is closest to
         the object in this relationship.
         """
+        possibilities = self.owner.getNeighboringLocations()
+        distances = [(self.getObjectDistance(x), x) for x in possibilities]
+        distances.sort()
+        return distances[0][1]
 
 class Object(object):
     pass
@@ -128,15 +157,19 @@ class Person(Object):
     """
 
     """
-    def __init__(self, id, location=(), relationships={}):
+    def __init__(self, id, container=None, location=(), relationships={}):
         """
 
         """
         self.id = self.name = id
+        self.container = container
         if location and isinstance(location, tuple):
             location = Location(*location)
         self.location = location
         self.relationships = relationships
+
+    def setContainer(self, container):
+        self.container = container
 
     def addRelationship(self, relationship):
         """
@@ -144,6 +177,25 @@ class Person(Object):
         """
         assert(relationship.owner == self)
         self.relationships[relationship.object] = relationship
+
+    def step(self, location):
+        self.container.moveObject(self, location)
+
+    def getNeighboringLocations(self):
+        """
+
+        """
+        locations = []
+        assert(self.location)
+        coords = list(self.location())
+        for index, coord in enumerate(self.location):
+            for change in [-1, 1]:
+                coords = list(self.location())
+                coords[index] = coords[index] + change
+                newLoc = Location(*coords)
+                if newLoc in self.container:
+                    locations.append(Location(*coords))
+        return locations
 
 
 def runTest():
@@ -156,9 +208,16 @@ def runTest():
     # bob is interested in being closer to alice
     bobToAlice = Relationship(bob, alice, 1)
     bob.addRelationship(bobToAlice)
-    print bob.relationships[alice].getObjectDistance()
     # alice is ambivalent to bob
     alliceToBob = Relationship(alice, bob, 0)
     alice.addRelationship(alliceToBob)
-    print alice.relationships[bob].getObjectDistance()
-
+    print "Alice: ", alice.location
+    print "Bob: ", bob.location
+    print "Distance: ", bob.relationships[alice].getObjectDistance()
+    # let's get bob moving
+    while bob.location not in alice.getNeighboringLocations():
+        loc = bob.relationships[alice].getNearestAdjacentLocation()
+        bob.step(loc)
+    print "Alice: ", alice.location
+    print "Bob: ", bob.location
+    print "Distance: ", bob.relationships[alice].getObjectDistance()
