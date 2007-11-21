6aff0f0bd61b58479b394061bf6b26dc40dfa79f
Duncan McGreggor <oubiwann@twistedmatrix.com>
2007-11-21 06:24:12 +0000

    Fixed up emotion interfaces (mostly whitespace adjustments).

--- sandbox/oubiwann/behavior/emotions.py
+++ sandbox/oubiwann/behavior/emotions.py
@@ -11,5 +11,6 @@ class ObjectPreference(ConcernTree):
     pass
 
 class EmotionalAppraisal:
+    pass
     
 
--- trunk/lib/emotion/interfaces.py
+++ trunk/lib/emotion/interfaces.py
@@ -1,6 +1,7 @@
 class IEmotionalIntensity:
     '''
-    Intensity of emotion. See the paper "How Emotion and Personality Effect the Utility of Alternate Decisions."
+    Intensity of emotion. See the paper "How Emotion and Personality Effect the
+    Utility of Alternate Decisions."
     '''
     def iHappyFor():
         '''
@@ -78,23 +79,31 @@ class IPhysioStressSubsystem:
 class IEmotionalAppraisal:
     '''
     Ie(Sk) = intensity of emotion e due to the kth state of the world
-    Je = the set of all agents relevant to emotion e. J1 is the set consisting only of the self. J2 is the set consisting of everyone but the self. J is the union of J1 and J2
-    Cijkl = a list of paths through the ith ontology of agent j triggered to condition l (0=success or 1=failure) by stake k
-    Wij(Cijkl) = the weighted importance of of the values of agent j that succeed and fail in one's ith concern set
-    f1(rjk) = a function that captures the stringth of positive and negative relationships one has with the ja agents of objects that are effected or spared in state k
-    f2(O,N) = a function that captures the temporal factors of the state and how to discount and merge one's emotions from the past, in the present and for the future.
-    e = emtional pairs; there are 11 pairs of these, oppositely valenced (e.g. pride-shame, hope-fear)
+    Je = the set of all agents relevant to emotion e. J1 is the set consisting
+        only of the self. J2 is the set consisting of everyone but the self. J
+        is the union of J1 and J2.
+    Cijkl = a list of paths through the ith ontology of agent j triggered to
+        condition l (0=success or 1=failure) by stake k.
+    Wij(Cijkl) = the weighted importance of of the values of agent j that
+        succeed and fail in one's ith concern set.
+    f1(rjk) = a function that captures the strength of positive and negative
+        relationships one has with the ja agents of objects that are effected
+        or spared in state k.
+    f2(O,N) = a function that captures the temporal factors of the state and
+        how to discount and merge one's emotions from the past, in the present
+        and for the future.
+    e = emtional pairs; there are 11 pairs of these, oppositely valenced (e.g.
+        pride-shame, hope-fear).
 
     Equation:
 
-    Ie(Sk) = Sum over each j belonging to Je of the Sum over each c belonging to Cijkl the product ( Wij(c) * f1(rjk) * f2(O,N) )
+    Ie(Sk) = Sum over each j belonging to Je of the Sum over each c belonging
+        to Cijkl the product ( Wij(c) * f1(rjk) * f2(O,N) )
 
-    Notes:
-
-    agents have three types of concern tree about the world:
-    1) goals for action
-    2) standards that people should follow, and
-    3) preferences for objects
+    Agents have three types of concern tree about the world:
+     1) goals for action
+     2) standards that people should follow, and
+     3) preferences for objects
 
     Sample usage:
 
@@ -188,4 +197,9 @@ class IOCCObjectAspect:
     '''
     '''
 
+class IPhysioStressSubsystem:
+    '''
+
+    '''
+    pass
 
