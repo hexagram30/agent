49d42edcdd58dc31a77f6b8c5d5947aeb1870bda
Duncan McGreggor <duncan@cogitat.io>
2013-11-10 13:55:08 -0600

    Added first modules and unit tests.

---
+++ test/simulacrum/version_test.clj
@@ -0,0 +1,13 @@
+(ns simulacrum.version-test
+  (:require [clojure.test :refer :all]
+            [simulacrum.version :as version]))
+
+
+(deftest test-version
+  (is (= 0 (version/version :major)))
+  (is (= 5 (version/version :minor)))
+  (is (= 0 (version/version :patch)))
+  (is (= true (version/version :snapshot))))
+
+(deftest test-version-str
+  (is (= "v0.5.0-dev" version/version-str)))
