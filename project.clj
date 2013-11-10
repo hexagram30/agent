6987364a45e7d2a6b290c0cdf07edd8565e5acd5
Duncan McGreggor <duncan@cogitat.io>
2013-11-10 13:53:16 -0600

    Added a project file for Clojure.

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject clj-simulacrum "0.5.0-SNAPSHOT"
+  :description "A Clojure library for exploring personality modeling in
+                simulated populations"
+  :url "https://github.com/oubiwann/clj-simulacrum"
+  :license {:name "The BSD 3-Clause License"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [co.paralleluniverse/pulsar "0.3.0"]]
+  :java-agents [[co.paralleluniverse/quasar-core "0.3.0"]]
+  :plugins [[lein-exec "0.3.1"]]
+  :profiles {
+    :dev {
+      :dependencies [[org.clojure/tools.namespace "0.2.3"]
+                     [org.clojure/java.classpath "0.2.0"]]}
+    :testing {
+      :dependencies [[leiningen "2.3.3"]]}})
