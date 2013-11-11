8fa0cb29ae80c3b04600d8ace2462cdc2eabf63b
Duncan McGreggor <duncan@cogitat.io>
2013-11-10 20:54:24 -0600

    Added new utility functions and updated input function.

--- src/simulacrum/util.clj
+++ src/simulacrum/util.clj
@@ -1,4 +1,6 @@
-(ns simulacrum.util)
+(ns simulacrum.util
+  (:require [clojure.string :as string]
+            [simulacrum.exceptions :as exceptions]))
 
 
 (defn display [data]
@@ -17,6 +19,34 @@
   (display (char 7))
   (clear-screen))
 
+(defn in?
+  "Given a sequence and a potential element of that sequence, determine if it
+  is, in fact, part of that sequence."
+  [sequence item]
+  (if (empty? sequence)
+    false
+    (reduce
+      #(or %1 %2)
+      (map
+        #(= %1 item)
+        sequence))))
+
+(defn check-input [input]
+  (let [valid-range (range 1 6)]
+    (cond
+      (in? valid-range input)
+        input
+      :else (throw
+              (exceptions/range-error
+                (str "Input must be between " (first valid-range) " and "
+                     (last valid-range)))))))
+
 (defn input [prompt]
   (display prompt)
-  (read-line))
+  (check-input (read-line)))
+
+(defn get-last-line [text]
+  (string/trim
+    (last
+      (string/split text #"\n"))))
+
