b3948a981bdc2e8521ff5a1586d4a4900b4a5bf8
Duncan McGreggor <duncan@cogitat.io>
2013-11-10 23:24:07 -0600

    Added code for doing inventories.

---
+++ src/simulacrum/inventory.clj
@@ -0,0 +1,94 @@
+(ns simulacrum.inventory
+  (:require [simulacrum.bigfive :as bigfive]
+            [simulacrum.exceptions :as exceptions]
+            [simulacrum.util :as util]))
+
+
+(defn display-title [questions]
+  (let [title (questions :title)
+        heading (util/mult-str "=" (count title))]
+  (util/display (str heading \newline))
+  (util/display (str title \newline))
+  (util/display (str heading \newline))
+  (util/display \newline)))
+
+(defn display-instructions [questions]
+  (util/display (questions :instructions))
+  (util/display \newline))
+
+(defn get-answer [prefix question]
+  (util/display (str "\"" prefix (question :question) ".\"" \newline))
+  (let [answer (util/input (str "Enter one of " (into [] (range 1 6)) " "))]
+    (util/display \newline)
+    (cond
+      (question :reversed?)
+        [(question :type) (- 6 answer)]
+      :else
+        [(question :type) answer])))
+
+(defn get-groups
+  "This is used when processing the results."
+  [results]
+  (group-by first results))
+
+(defn gather-subgroups
+  "This is used when processing the results."
+  [subgroup]
+  (map second (second subgroup)))
+
+(defn average-tuple
+  "This is used when processing the results."
+  [tuple]
+  (/ (reduce + tuple) (float (count tuple))))
+
+(defn group-average
+  "Given a group, determine the average and associate it with its type."
+  [group]
+  [(first group) (average-tuple (gather-subgroups group))])
+
+(defn process-results [results]
+  (into {} 
+        (map 
+          group-average
+          (get-groups results))))
+
+(defn display-score [averages]
+  (let [title "Inventory Results"
+        heading (util/mult-str "=" (count title))]
+    (util/display (str heading \newline))
+    (util/display (str title \newline))
+    (util/display (str heading \newline \newline))
+    (doseq [[key value] averages]
+      (util/display
+        (str \tab (bigfive/attributes key) ": " value \newline)))))
+
+(defn -run-inventory [questions]
+  (let [prefix (questions :prefix)]
+    (display-title questions)
+    (display-instructions questions)
+    (util/display \newline)
+    (display-score
+      (process-results
+        (map #(get-answer prefix %) (questions :questions))))))
+
+(defn run-inventory
+  "The required positional parameter (not one of the named parameters) needs to
+  be a keyword that maps to a defined module for personality traits. For
+  instance, if simulacrum.bigfive is defined (and it is) and you wanted to run
+  the inventory for it, you'd pass :bigfive.
+  
+  Right now, the list of supported personality trait frameworks are the
+  following:
+    * :bigfive
+  
+  One of ':short true' or ':long true' must be passed to this function."
+  [type-keyword & {:keys [short long]}]
+  (let [type (name type-keyword)]
+      (cond
+        (not (nil? short))
+          (-run-inventory (eval (symbol type "questions-short")))
+        (not (nil? long))
+          (-run-inventory (eval (symbol type "questions-long")))
+        :else (throw
+                (exceptions/param-error 
+                  (util/get-last-line ((meta #'run-inventory) :doc)))))))
---
+++ test/simulacrum/inventory_test.clj
@@ -0,0 +1,38 @@
+(ns simulacrum.inventory-test
+  (:require [clojure.test :refer :all]
+            [simulacrum.bigfive :as bigfive]
+            [simulacrum.inventory :as inventory]))
+
+
+(deftest test-run-inventory-no-short-no-long
+  (is (thrown-with-msg?
+        clojure.lang.ExceptionInfo
+        #"One of ':short true' or ':long true' must be passed to this function"
+        (inventory/run-inventory :bigfive))))
+
+(deftest test-get-groups
+  (let [data [[:E 5] [:A 2] [:C 3] [:N 2] [:O 1]
+              [:E 1] [:A 4] [:C 1] [:N 1] [:O 4]]]
+    (is (= {:E [[:E 5] [:E 1]], :A [[:A 2] [:A 4]], :C [[:C 3] [:C 1]], 
+            :N [[:N 2] [:N 1]], :O [[:O 1] [:O 4]]}
+           (inventory/get-groups data)))))
+
+(deftest test-gather-subgroups
+  (let [data [:E [[:E 5] [:E 1]]]]
+    (is (= [5 1]
+           (inventory/gather-subgroups data)))))
+
+(deftest test-average-tuple
+  (is (= 3.0 (inventory/average-tuple [1 5])))
+  (is (= 2.5 (inventory/average-tuple [1 4])))
+  (is (= 3.0 (inventory/average-tuple [2 4]))))
+
+(deftest test-process-results
+  (let [data [[:E 5] [:A 2] [:C 3] [:N 2] [:O 1]
+              [:E 5] [:A 2] [:C 3] [:N 2] [:O 1]]]
+    (is (= {:E 5.0, :A 2.0, :C 3.0, :N 2.0, :O 1.0}
+           (inventory/process-results data))))
+  (let [data [[:E 5] [:A 2] [:C 3] [:N 2] [:O 1]
+              [:E 1] [:A 4] [:C 1] [:N 1] [:O 4]]]
+    (is (= {:E 3.0, :A 3.0, :C 2.0, :N 1.5, :O 2.5}
+           (inventory/process-results data)))))
