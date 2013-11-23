b53d21f4dc66d9a429de0a3c2cf2caf0a59d251a
Duncan McGreggor <duncan@cogitat.io>
2013-11-23 13:23:00 -0600

    Added missing unit tests.

--- test/simulacrum/util_test.clj
+++ test/simulacrum/util_test.clj
@@ -3,13 +3,6 @@
             [simulacrum.util :as util]))
 
 
-(deftest test-get-last-line
-  (let [test-data "This is some
-                  multi-line
-                  text.
-                  Enjoy!"]
-    (is (= "Enjoy!" (util/get-last-line test-data)))))
-
 (deftest test-mult-str
   (is (= "ab-ab-ab-ab-" (util/mult-str "ab-" 4))))
 
@@ -32,3 +25,24 @@
         clojure.lang.ExceptionInfo
         #"Input must be between 1 and 5"
         (util/check-input 6))))
+
+(deftest test-get-last-line
+  (let [test-data "This is some
+                  multi-line
+                  text.
+                  Enjoy!"]
+    (is (= "Enjoy!" (util/get-last-line test-data)))))
+
+(deftest test-user-agent
+  (is (= "clj-simulacrum v0.2-dev (https://github.com/oubiwann/clj-simulacrum)"
+         util/user-agent)))
+
+(deftest test-ua-headers
+  (is (= {:headers
+          {"User-Agent"
+           "clj-simulacrum v0.2-dev (https://github.com/oubiwann/clj-simulacrum)"}}
+         util/ua-headers)))
+
+(deftest test-remove-spaces-and-newlines
+  (let [test-data "This is some \n Enjoy!"]
+    (is (= "This is some Enjoy!" (util/remove-spaces-and-newlines test-data)))))
