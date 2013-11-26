5e7c2f057d4767653b04dd7fcd798c224d3141ff
Duncan McGreggor <duncan@cogitat.io>
2013-11-25 23:26:33 -0600

    Added new function for doing matrix analysis.

--- src/simulacrum/math.clj
+++ src/simulacrum/math.clj
@@ -26,8 +26,8 @@
   Note that the personality matrices are 1x5 matrices and the resultant matrix
   is the same shape (1x5)."
   (matrix/emap abs
-          (sub pers-matrix-1
-               pers-matrix-2)))
+               (sub pers-matrix-1
+                    pers-matrix-2)))
 
 (defn get-inverted-matrix-difference
   [pers-matrix-1 pers-matrix-2]
@@ -41,18 +41,18 @@
          pers-matrix-2)))
 
 (defn normalize-matrix
-  "Given a matrix, normalize it either by its dimension or by the largest
-  element.
+  "Given a matrix, normalize it either by the largest value in the shape vector
+  (:maxsize) or by the largest element in the matrix (:largest).
 
   'normal-mode' is the method used to select the normalization value. If the
   value of 'normal-mode' is :rank, the matrix rank is used to normalize the
   values in the matrix. If it is :largest, the largest value of the matrix is
   given as the rank."
   ([matrix-data]
-   (normalize-matrix matrix-data :dimension))
+   (normalize-matrix matrix-data :maxsize))
   ([matrix-data normal-mode]
     (cond
-      (= normal-mode :dimension)
+      (= normal-mode :maxsize)
         (div matrix-data (last (matrix/shape matrix-data)))
       (= normal-mode :largest)
         (div matrix-data (apply max (flatten matrix-data))))))
@@ -65,7 +65,7 @@
   values in the matrix. If it is :largest, the largest value of the matrix is
   given as the rank."
   ([matrix-1 matrix-2]
-   (get-normalized-matrix matrix-1 matrix-2 :dimension))
+   (get-normalized-matrix matrix-1 matrix-2 :maxsize))
   ([matrix-1 matrix-2 normal-mode]
     (let [matrix (matrix/mmul matrix-1 matrix-2)]
       (normalize-matrix matrix normal-mode))))
@@ -80,8 +80,23 @@
   given as the rank."
   ([pers-matrix-1 pers-matrix-2 model]
    (compute-compatibility-matrix
-     pers-matrix-1 pers-matrix-2 model :dimension))
+     pers-matrix-1 pers-matrix-2 model :maxsize))
   ([pers-matrix-1 pers-matrix-2 model normal-mode]
     (let [pers-combo (matrix/mmul (matrix/transpose pers-matrix-1) pers-matrix-2)
           compat-combo (matrix/mmul pers-combo model)]
       (normalize-matrix compat-combo normal-mode))))
+
+(defn get-transpose-average
+  [matrix-data]
+  "Add the given matrix to its transpose and then divide by two.
+
+  This is useful for forming diagonal matrices from personality matrix
+  multiplication results."
+  (div (add
+         (matrix/transpose matrix-data)
+         matrix-data)
+       2))
+
+
+
+
--- test/simulacrum/math_test.clj
+++ test/simulacrum/math_test.clj
@@ -1,18 +1,35 @@
 (ns simulacrum.math-test
   (:require [clojure.test :refer :all]
             [clojure.core.matrix :as matrix]
-            [simulacrum.math :as math]))
+            [clojure.core.matrix.operators]
+            [simulacrum.math :as math])
+  (:refer clojure.core.matrix.operators :rename
+          {/ div
+           * mult
+           ** pow
+           + add
+           - sub
+           == eql}))
 
 
-(def person-1
+(def alice
+  (matrix/matrix [[0.98M 0.64M 0.76M 0.98M 0.93M]]))
+
+(def bob
+  (matrix/matrix [[1.0M 0.9M 0.8M 0.7M 0.4M]]))
+
+(def carol
+  (matrix/matrix [[0.02M 0.36M 0.24M 0.02M 0.07M]]))
+
+(def dave
   (matrix/matrix [[0.7M 0.5M 0.9M 0.8M 0.1M]]))
 
-(def person-2
+(def eve
   (matrix/matrix [[0.1M 0.9M 0.1M 0.7M 0.8M]]))
 
-(def person-3 [[1 4]])
+(def frank [[1 4]])
 
-(def person-4 [[3 4]])
+(def gina [[3 4]])
 
 (def matrix-1
   (matrix/matrix [[1 2]
@@ -25,21 +42,27 @@
 
 (deftest test-get-scalar-distance
   (is (= 0.0
-         (math/get-scalar-distance person-1 person-1)))
-  (is (= 1.2884098726725126
-         (math/get-scalar-distance person-1 person-2))))
+         (math/get-scalar-distance alice alice)))
+  (is (= 0.6549045732013177
+         (math/get-scalar-distance alice bob)))
+  (is (= 1.7121915780659593
+         (math/get-scalar-distance alice carol)))
+  (is (= 0.9159148432032314
+         (math/get-scalar-distance alice dave)))
+  (is (= 1.1717081547894084
+         (math/get-scalar-distance alice eve))))
 
 (deftest test-get-matrix-difference
   (is (= [[0.0M 0.0M 0.0M 0.0M 0.0M]]
-         (math/get-matrix-difference person-1 person-1)))
+         (math/get-matrix-difference dave dave)))
   (is (= [[0.6M 0.4M 0.8M 0.1M 0.7M]]
-         (math/get-matrix-difference person-1 person-2))))
+         (math/get-matrix-difference dave eve))))
 
 (deftest test-get-inverted-matrix-difference
   (is (= [[1.0M 1.0M 1.0M 1.0M 1.0M]]
-         (math/get-inverted-matrix-difference person-1 person-1)))
+         (math/get-inverted-matrix-difference eve eve)))
   (is (= [[0.4M 0.6M 0.2M 0.9M 0.3M]]
-         (math/get-inverted-matrix-difference person-1 person-2))))
+         (math/get-inverted-matrix-difference dave eve))))
 
 (deftest test-normalize-matrix
   (is (= [[1/2 1]
@@ -47,7 +70,7 @@
          (math/normalize-matrix matrix-1)))
   (is (= [[1/2 1]
           [3/2 2]]
-         (math/normalize-matrix matrix-1 :dimension)))
+         (math/normalize-matrix matrix-1 :maxsize)))
   (is (= [[1/4 1/2]
           [3/4 1]]
          (math/normalize-matrix matrix-1 :largest)))
@@ -66,7 +89,7 @@
          (math/get-normalized-matrix matrix-1 matrix-1)))
   (is (= [[7/2 5]
           [15/2 11]]
-         (math/get-normalized-matrix matrix-1 matrix-1 :dimension)))
+         (math/get-normalized-matrix matrix-1 matrix-1 :maxsize)))
   (is (= [[7/22 5/11]
           [15/22 1]]
          (math/get-normalized-matrix matrix-1 matrix-1 :largest))))
@@ -74,12 +97,23 @@
 (deftest test-compute-compatibility-matrix
   (is (= [[15/2 11]
           [30 44]]
-         (math/compute-compatibility-matrix person-3 person-4 matrix-1)))
+         (math/compute-compatibility-matrix frank gina matrix-1)))
   (is (= [[15/2 11]
           [30 44]]
          (math/compute-compatibility-matrix
-           person-3 person-4 matrix-1 :dimension)))
+           frank gina matrix-1 :maxsize)))
   (is (= [[15/88 1/4]
           [15/22 1]]
          (math/compute-compatibility-matrix
-           person-3 person-4 matrix-1 :largest))))
+           frank gina matrix-1 :largest))))
+
+(deftest test-get-transpose-average
+  (let [couple (matrix/mmul (matrix/transpose alice) bob)
+        expected [[0.980 0.761 0.772 0.833 0.661]
+                  [0.761 0.576 0.598 0.665 0.5465]
+                  [0.772 0.598 0.608 0.658 0.524]
+                  [0.833 0.665 0.658 0.686 0.5215]
+                  [0.661 0.5465 0.524 0.5215 0.372]]
+        result (math/get-transpose-average couple)]
+    (is (= (matrix/new-matrix 5 5) (sub result expected)))))
+
